f7889324c568a3b38b01ae955fb09d19bce25359
Lorenzo Bianconi <lorenzo@kernel.org>
2025-12-01 10:05:16 +0100

    update config for 7581-eagle
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- target/linux/airoha/base-files/etc/trig-ser.sh
+++ target/linux/airoha/base-files/etc/trig-ser.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 DIR=/sys/kernel/debug/ieee80211/phy0/mt76/
+[ $# -ge 1 ] BAND=$1 || BAND=1
 
 echo 0x00401484 > $DIR/regidx
 echo 0x15100002 > $DIR/regval
-echo '0,1' > $DIR/sys_recovery
+echo "$BAND,1" > $DIR/sys_recovery
