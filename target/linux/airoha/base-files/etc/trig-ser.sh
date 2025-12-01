014d56c297c11b7168216c201984acbb4536d27f
Lorenzo Bianconi <lorenzo@kernel.org>
2025-12-01 10:24:55 +0100

    aiorha: fix trig-ser.sh

--- target/linux/airoha/base-files/etc/trig-ser.sh
+++ target/linux/airoha/base-files/etc/trig-ser.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/sh -x
 
 DIR=/sys/kernel/debug/ieee80211/phy0/mt76/
-[ $# -ge 1 ] BAND=$1 || BAND=1
+[ $# -ge 1 ] && BAND=$1 || BAND=1
 
 echo 0x00401484 > $DIR/regidx
 echo 0x15100002 > $DIR/regval
